a1bd9a6fb24d7bb3b47370206c0c29f71151b92e
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-09-28 17:18:10 -0300

    Adding some css to the campaigns page with semantic-ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
@@ -8,8 +9,33 @@ class CampaignIndex extends Component {
         return { campaigns }
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true // It covers all the container's width
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
-        return <div>{this.props.campaigns[0]}</div>
+        return (
+        <div>
+            <link
+                rel="stylesheet"
+                href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+            ></link>
+            {this.renderCampaigns()}
+            <Button 
+                content="Create Campaign"
+                icon="add circle"
+                primary
+            />
+        </div>
+        );
     }
 }
 

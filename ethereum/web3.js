49d6a4c188ff5d8899cb8659e31590f19a24ce79
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-09-28 12:49:44 -0300

    Fixing window variable to be used when Metamask is on

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,17 @@
-import Web3 from 'web3';
-
-window.ethereum.request({ method: "eth_requestAccounts" });
-
-const web3 = new Web3(window.ethereum);
-
+import Web3 from "web3";
+ 
+let web3;
+ 
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // We are on the server *OR* the user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/259164015f264f53842f61f50488a30f"
+  );
+  web3 = new Web3(provider);
+}
+ 
 export default web3;

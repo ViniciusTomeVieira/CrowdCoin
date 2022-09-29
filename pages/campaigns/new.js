405a13a31cb68dca899686771737912520c143da
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-09-29 13:10:11 -0300

    Calling the factory campaign contract in the app

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import { Form, Button, Input} from 'semantic-ui-react';
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
         minimumContribution: ''
     };
 
+    onSubmit = async (event) => {
+        event.preventDefault(); //This makes the browser to not submit the form
+
+        const accounts = await web3.eth.getAccounts();
+        await factory.method.createCampaign(this.state.minimumContribution).send({
+            from: accounts[0] // This dont require any gas, because metamask do the math for us
+        });
+    }
+
     render () {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
 
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input

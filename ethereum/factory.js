771f3a532f8b4d7c910584877a4db155c80d3bdb
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-10-04 12:50:22 -0300

    Deploying a new Campaign Factory Contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xb79D3DDFF822cB8B6fb8907e00B9635ac84dc9C6'
+    '0xA37C7fE8e6CCf2E1DA9f90EDc221d9C35983DD57'
 );
 
 export default instance;

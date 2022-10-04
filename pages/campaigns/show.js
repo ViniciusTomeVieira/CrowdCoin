fc50ef5187ca705a91c1ec5b4c72dbb75a5d2076
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-10-04 12:59:29 -0300

    Getting the contract values into the show page

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
-    render() {
 
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.GetSummary().call();
+        
+    }
+
+    render() {
         return (
             <Layout>
                 <h3>Campaign Details</h3>

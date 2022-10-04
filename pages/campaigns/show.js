5bac99e5d29c38ebd70429102e67fc47421da9f6
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-10-04 13:04:24 -0300

    Returning the contract values to the show page props

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -7,7 +7,15 @@ class CampaignShow extends Component {
     static async getInitialProps(props) {
         const campaign = Campaign(props.query.address);
         const summary = await campaign.methods.GetSummary().call();
-        
+
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        }
+
     }
 
     render() {

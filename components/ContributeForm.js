c3ed5a5f4b0542683455bc92fc02cfb882d27a33
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-10-05 12:21:36 -0300

    Setting the campaign address to the contribute form

---
+++ components/ContributeForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+import Campaign from '../ethereum/campaign';
+
+class ContributeForm extends Component {
+
+    state = {
+        value: ''
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+
+        const campaign = Campaign(this.props.address);
+    }
+
+    render () {
+        return (
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field>
+                    <label>Amount to contribute</label>
+                    <Input
+                        value={this.state.value}
+                        onChange={event => this.setState({ value: event.target.value})}
+                        label="ether"
+                        labelPosition='right'
+                    />
+                </Form.Field>
+                <Button primary>
+                    Contribute!
+                </Button>
+            </Form>
+        );
+    }
+}
+
+export default ContributeForm;

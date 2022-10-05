bfafe62089c29f77a78cff29304c80297c358879
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2022-10-05 12:27:08 -0300

    Getting value from the contribute form

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
 
 class ContributeForm extends Component {
 
@@ -8,10 +9,20 @@ class ContributeForm extends Component {
         value: ''
     }
 
-    onSubmit = (event) => {
+    onSubmit = async event => {
         event.preventDefault();
 
         const campaign = Campaign(this.props.address);
+
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+        } catch (err) {
+
+        }
     }
 
     render () {
